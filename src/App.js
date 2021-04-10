5bb4b932d2348c2bf744214b6ab1b0a35cf9593f
shourov-sorkar <18103142@iubat.edu>
2021-04-10 17:47:26 +0600

    :boom: Show data in frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FetchJobs from './Components/FetchJobs'
 import {Container} from 'react-bootstrap'
-
+import Job from './Components/Job'
 function App() {
-  const { jobs, loading, error } = FetchJobs()
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error } = FetchJobs(params, page)
   return (
     <Container>
       {loading && <h1>Loading....</h1>}
       {error && <h1>Error Try again....</h1>}
-      {<h1>{jobs.length}</h1>}
+      {jobs.map(job =>{
+        return <Job key={job.id} job={job}/>
+      })}
     </Container>
   )
 }
